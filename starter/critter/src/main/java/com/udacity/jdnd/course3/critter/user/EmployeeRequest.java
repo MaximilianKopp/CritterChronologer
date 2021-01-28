a31e8fbbde6b96960d8e50f4e76a327415ff7149
mkopp <m.kopp89@web.de>
2021-01-28 14:08:32 +0100

    Create Projection for EmployeeRequest

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/EmployeeRequest.java
@@ -0,0 +1,28 @@
+package com.udacity.jdnd.course3.critter.user;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+/**
+ * Does not need to be an entity since we do not need to persist it to the database.
+ */
+public class EmployeeRequest {
+    private Set<EmployeeSkill> skills;
+    private LocalDate date;
+
+    public Set<EmployeeSkill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(Set<EmployeeSkill> skills) {
+        this.skills = skills;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
